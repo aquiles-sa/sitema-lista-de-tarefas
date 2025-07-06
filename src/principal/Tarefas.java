64948f173ee538a0c00ba10a5d033e597baa9588
Aquiles <aquiles2ws@gmail.com>
2025-07-06 15:08:13 -0300

    ft: Criada o método de edição de tarefa.

--- src/principal/Tarefas.java
+++ src/principal/Tarefas.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Tarefas {
 	private String tarefa;
-	public ArrayList<String> tarefas = new ArrayList<>();
+	private ArrayList<String> tarefas = new ArrayList<>();
 	
 	public Tarefas() {
 		this.tarefa = null;
@@ -18,6 +18,12 @@ public class Tarefas {
 		this.tarefa = nova_tarefa;
 	}
 	
+	public void mostrarTarefas () {
+		for (int i = 0; i < tarefas.size(); i++) {
+			System.out.println(i + 1 + " - " + tarefas.get(i));
+		}
+	}
+	
 	public void adicionarTarefa (Scanner sc) {
 		String tarefa_adicionada;
 		
@@ -28,9 +34,34 @@ public class Tarefas {
 		System.out.println("A tarefa foi adicionada com sucesso.");
 	}
 	
-	public void mostrarTarefas () {
-		for (int i = 0; i < tarefas.size(); i++) {
-			System.out.println(i + 1 + " - " + tarefas.get(i));
+	public void editarTarefa (Scanner sc) {
+		int tarefa_a_ser_editada;
+		String tarefa_atualizada;
+		
+		System.out.print("Digite o índice da tarefa a ser editada: ");
+		tarefa_a_ser_editada = sc.nextInt();
+		sc.nextLine();
+		System.out.println();
+		
+		if (tarefa_a_ser_editada < 0 || tarefa_a_ser_editada >= tarefas.size()) {
+			System.out.println("Essa tarefa não existe.");
+		} else {
+			String tarefa_original = tarefas.get(tarefa_a_ser_editada);
+			
+			System.out.println("Tarefa a ser editada: " + tarefa_original);
+			System.out.print("Escreva a tarefa atualizada: ");
+			tarefa_atualizada = sc.nextLine();
+			
+			tarefas.set(tarefa_a_ser_editada, tarefa_atualizada);
+			System.out.println("Tarefa atualizada com sucesso!");
 		}
-	}	
+		
+	}
 }
+
+
+
+
+
+
+
