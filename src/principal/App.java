a50cc20e1a5860581bc6bbec64f95268771e354a
Aquiles <aquiles2ws@gmail.com>
2025-07-06 14:32:58 -0300

    wip: Cria método de menu.

--- src/principal/App.java
+++ src/principal/App.java
@@ -12,15 +12,50 @@ public class App {
 		boolean login_efetizado = loginView.realizarLogin(scan, cadastro); 
 		
 		Tarefas tarefas = new Tarefas();
-		
+	
 		if (login_efetizado) {
 			System.out.println("Acesso ao sistema permitido!");
-			tarefas.adicionarTarefa(scan);
-			tarefas.mostrarTarefas();
+			imprimirMenu(scan, cadastro, tarefas);
 			
 		} else {
 			System.out.println("Acesso negado.");
 		}
-	}
 
+	}
+	
+	public static void imprimirMenu(Scanner sc, Cadastro cadastro, Tarefas tarefas) {
+		int opcao = 0;
+		
+		while(opcao != 6) {
+			System.out.println("-------- MENU " + cadastro.getNome() + " --------");
+			System.out.println("1 - Mostrar lista completa.");
+			System.out.println("2 - Adicionar nova tarefa.");
+			System.out.println("3 - Editar tarefa.");
+			System.out.println("4 - Excluir tarefa.");
+			System.out.println("5 - Limpar lista.");
+			System.out.println("6 - Sair. \n");
+			
+			opcao = sc.nextInt();
+			sc.nextLine();
+			
+			switch (opcao) {
+				case 1: {
+					tarefas.mostrarTarefas();
+					break;
+				}
+				
+				case 2: 
+					tarefas.adicionarTarefa(sc);
+					break;
+				
+				case 6:
+					System.out.println("Programa encerrado.");
+					break;
+				default:
+					throw new IllegalArgumentException("Opção inválida: " + opcao);
+			}
+			
+		};
+		
+	}
 }
